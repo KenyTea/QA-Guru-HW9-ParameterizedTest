20629b60226198a55f1f33563921ff743392e532
Alexey <vassilyevalexey@gmail.com>
2021-11-17 09:59:33 +0600

    Optimization and add variables

--- src/test/java/guru/qa/HW9_ParameterizedTest.java
+++ src/test/java/guru/qa/HW9_ParameterizedTest.java
@@ -1,6 +1,8 @@
 package guru.qa;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -14,16 +16,23 @@ import static org.openqa.selenium.By.linkText;
 
 public class HW9_ParameterizedTest {
 
+    String URL = "https://github.com/qa-guru";
+    SelenideElement reposFilter = $("#your-repos-filter");
+    ElementsCollection repositorySearchResult = $$("a");
+    ElementsCollection checkingTextOnPage = $$("Box-header position-relative");
+    String repoName = "qa_guru_9_8_files";
+    String checkingText = "JUnit 5 examples";
+
     @DisplayName("Поиск в github репозитория qa_guru_9_8_files")
     @Tag("blocker")
     @Test
     void selenideSearchTest() {
-        open("https://github.com/qa-guru");
-        $("#your-repos-filter").scrollTo();
-        $("#your-repos-filter").setValue("qa_guru_9_8_files");
-        // $("#your-repos-filter").click();
-        $$("a").find(Condition.text("qa_guru_9_8_files")).click();
-        $$("Box-header position-relative").find(Condition.text("JUnit 5 examples"));
+        open(URL);
+        reposFilter
+                .scrollTo()
+                .setValue(repoName);
+        repositorySearchResult.find(Condition.text(repoName)).click();
+        checkingTextOnPage.find(Condition.text(checkingText));
 
     }
 }
