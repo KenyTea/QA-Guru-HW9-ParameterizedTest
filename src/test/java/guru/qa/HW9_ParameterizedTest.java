5fc425dacb5f25b87a72c47e88b05d74b36d8819
Alexey <vassilyevalexey@gmail.com>
2021-11-21 15:06:04 +0600

    Коментарий 2

--- src/test/java/guru/qa/HW9_ParameterizedTest.java
+++ src/test/java/guru/qa/HW9_ParameterizedTest.java
@@ -39,7 +39,7 @@ public class HW9_ParameterizedTest {
     void repositorySearchTest() {
         open(pageObject.URL); // Открываем браузер
         pageObject.reposFilter.scrollTo().setValue(pageObject.repoName); // Скролим до поисковой строки и вбиваем название репозитория
-        pageObject.repositorySearchResult.find(text(pageObject.repoName)).click();
+        pageObject.repositorySearchResult.find(text(pageObject.repoName)).click(); // Кликаем на найденный репозиторий
         pageObject.checkingTextOnPage.shouldHave(text(pageObject.checkingText));
     }
 
